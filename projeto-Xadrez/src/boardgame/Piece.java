d620feb415ce609d7c40416dd48890f3e2a008d3
enonnemacher <enonnemacher@gmail.com>
2021-11-29 09:53:54 -0300

    Possible moves of a piece

--- projeto-Xadrez/src/boardgame/Piece.java
+++ projeto-Xadrez/src/boardgame/Piece.java
@@ -1,7 +1,7 @@
 package boardgame;
 
 // classe responsável pelo tabuleiro
-public class Piece {
+public abstract class Piece {
 
     protected Position position;
     private Board board;
@@ -13,5 +13,24 @@ public class Piece {
 
     protected Board getBoard() {
         return board;
+    }
+
+    // verifica os movimentos possíveis no tabuleiro
+    public abstract boolean[][] possibleMoves();
+
+    public boolean possibleMove(Position position) {
+        return possibleMoves()[position.getRow()][position.getColumn()];
+    }
+
+    public boolean isThereAnyPossibleMove() {
+        boolean[][] possible = possibleMoves();
+        for (int i = 0; i < possible.length; i++) {
+            for (int j = 0; j < possible.length; j++) {
+                if (possible[i][j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
