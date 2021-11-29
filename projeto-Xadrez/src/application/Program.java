08ad14aa415a235df5171325d6989bb24292edda
enonnemacher <enonnemacher@gmail.com>
2021-11-29 09:25:52 -0300

    Handling exceptions and clearing screen

--- projeto-Xadrez/src/application/Program.java
+++ projeto-Xadrez/src/application/Program.java
@@ -1,9 +1,11 @@
 package application;
 
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Program {
@@ -12,16 +14,25 @@ public class Program {
         ChessMatch chessMatch = new ChessMatch();
 
         while (true) {
-            UI.printBoard(chessMatch.getPieces());
-            System.out.println();
-            System.out.println("Source: ");
-            ChessPosition source = UI.readChessPosition(leitorTeclado);
+            try {
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.println("Source: ");
+                ChessPosition source = UI.readChessPosition(leitorTeclado);
 
-            System.out.println();
-            System.out.println("Target: ");
-            ChessPosition target = UI.readChessPosition(leitorTeclado);
+                System.out.println();
+                System.out.println("Target: ");
+                ChessPosition target = UI.readChessPosition(leitorTeclado);
 
-            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            } catch (ChessException e) {
+                System.out.println(e.getMessage());
+                leitorTeclado.nextLine();
+            } catch (InputMismatchException e) {
+                System.out.println(e.getMessage());
+                leitorTeclado.nextLine();
+            }
         }
     }
 }
