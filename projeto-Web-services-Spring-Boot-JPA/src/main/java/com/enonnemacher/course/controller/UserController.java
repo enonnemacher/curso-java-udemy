7065fb525b8c57a5241f6fb3f2e8aae6a4bdaa69
enonnemacher <enonnemacher@gmail.com>
2022-01-03 16:44:56 -0300

    User entity and resource

---
+++ projeto-Web-services-Spring-Boot-JPA/src/main/java/com/enonnemacher/course/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.enonnemacher.course.controller;
+
+import com.enonnemacher.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+}
