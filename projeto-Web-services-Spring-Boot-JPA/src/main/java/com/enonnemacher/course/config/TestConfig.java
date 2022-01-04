8fe5de9ece42ba5c605945b952067058d4aeac47
enonnemacher <enonnemacher@gmail.com>
2022-01-04 16:00:43 -0300

    OrderStatus enum

--- projeto-Web-services-Spring-Boot-JPA/src/main/java/com/enonnemacher/course/config/TestConfig.java
+++ projeto-Web-services-Spring-Boot-JPA/src/main/java/com/enonnemacher/course/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.enonnemacher.course.config;
 
 import com.enonnemacher.course.entities.Order;
 import com.enonnemacher.course.entities.User;
+import com.enonnemacher.course.entities.enums.OrderStatus;
 import com.enonnemacher.course.repository.OrderRepository;
 import com.enonnemacher.course.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
         User usuario2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
         userRepository.saveAll(Arrays.asList(usuario1, usuario2));
 
-        Order order1 = new Order(null, Instant.parse("2021-12-20T19:53:07Z"), usuario1);
-        Order order2 = new Order(null, Instant.parse("2021-12-21T03:42:10Z"), usuario2);
-        Order order3 = new Order(null, Instant.parse("2021-12-22T15:21:22Z"), usuario1);
+        Order order1 = new Order(null, Instant.parse("2021-12-20T19:53:07Z"), OrderStatus.PAID,usuario1);
+        Order order2 = new Order(null, Instant.parse("2021-12-21T03:42:10Z"), OrderStatus.PAID,usuario2);
+        Order order3 = new Order(null, Instant.parse("2021-12-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT,usuario1);
         orderRepository.saveAll(Arrays.asList(order1, order2, order3));
     }
 }
