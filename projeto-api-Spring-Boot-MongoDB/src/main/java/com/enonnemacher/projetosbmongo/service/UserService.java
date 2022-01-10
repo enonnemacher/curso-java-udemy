7bc258e22f0397ee0b5e8b4cf7c8e9a834ddd116
enonnemacher <enonnemacher@gmail.com>
2022-01-10 16:09:02 -0300

    Insercao de usuario com POST

--- projeto-api-Spring-Boot-MongoDB/src/main/java/com/enonnemacher/projetosbmongo/service/UserService.java
+++ projeto-api-Spring-Boot-MongoDB/src/main/java/com/enonnemacher/projetosbmongo/service/UserService.java
@@ -1,6 +1,7 @@
 package com.enonnemacher.projetosbmongo.service;
 
 import com.enonnemacher.projetosbmongo.domain.User;
+import com.enonnemacher.projetosbmongo.dto.UserDTO;
 import com.enonnemacher.projetosbmongo.exception.NotFoundException;
 import com.enonnemacher.projetosbmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> user = userRepository.findById(id);
         return user.orElseThrow(() -> new NotFoundException("Objeto nao encontrado."));
+    }
+
+    public User insert(User user) {
+        return userRepository.insert(user);
+    }
+
+    public User fromDTO(UserDTO userDTO) {
+        return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
     }
 }
