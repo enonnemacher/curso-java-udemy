a64c9912ea71e36a5f10c0904402b146bd6ea011
enonnemacher <enonnemacher@gmail.com>
2022-01-10 17:30:15 -0300

    Obtendo um post por id

---
+++ projeto-api-Spring-Boot-MongoDB/src/main/java/com/enonnemacher/projetosbmongo/controller/PostController.java
@@ -0,0 +1,23 @@
+package com.enonnemacher.projetosbmongo.controller;
+
+import com.enonnemacher.projetosbmongo.domain.Post;
+import com.enonnemacher.projetosbmongo.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+
+    @Autowired
+    private PostService postService;
+
+    @RequestMapping("/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post post = postService.findById(id);
+        return ResponseEntity.ok().body(post);
+    }
+}
---
+++ projeto-api-Spring-Boot-MongoDB/src/main/java/com/enonnemacher/projetosbmongo/service/PostService.java
@@ -0,0 +1,19 @@
+package com.enonnemacher.projetosbmongo.service;
+
+import com.enonnemacher.projetosbmongo.domain.Post;
+import com.enonnemacher.projetosbmongo.exception.NotFoundException;
+import com.enonnemacher.projetosbmongo.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public Post findById(String id) {
+        Optional<Post> userPost = postRepository.findById(id);
+        return userPost.orElseThrow(() -> new NotFoundException("Objeto nao encontrado."));
+    }
+}
