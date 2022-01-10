0c03aa7ac6a20ff3141fff8a7553a16cc6b39544
enonnemacher <enonnemacher@gmail.com>
2022-01-10 14:35:33 -0300

    Projeto criado

---
+++ projeto-api-Spring-Boot-MongoDB/src/main/java/com/enonnemacher/projetosbmongo/ProjetosbmongoApplication.java
@@ -0,0 +1,13 @@
+package com.enonnemacher.projetosbmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetosbmongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetosbmongoApplication.class, args);
+    }
+
+}
---
+++ projeto-api-Spring-Boot-MongoDB/src/test/java/com/enonnemacher/projetosbmongo/ProjetosbmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enonnemacher.projetosbmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetosbmongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
