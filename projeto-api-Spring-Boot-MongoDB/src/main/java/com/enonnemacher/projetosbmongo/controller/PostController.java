b39b1d0d89fae48e4d304658376f6cf0759f8998
enonnemacher <enonnemacher@gmail.com>
2022-01-11 09:57:28 -0300

    Consulta simples com query methods

--- projeto-api-Spring-Boot-MongoDB/src/main/java/com/enonnemacher/projetosbmongo/controller/PostController.java
+++ projeto-api-Spring-Boot-MongoDB/src/main/java/com/enonnemacher/projetosbmongo/controller/PostController.java
@@ -1,13 +1,13 @@
 package com.enonnemacher.projetosbmongo.controller;
 
+import com.enonnemacher.projetosbmongo.controller.util.URL;
 import com.enonnemacher.projetosbmongo.domain.Post;
 import com.enonnemacher.projetosbmongo.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/posts")
@@ -20,5 +20,12 @@ public class PostController {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post post = postService.findById(id);
         return ResponseEntity.ok().body(post);
+    }
+
+    @GetMapping("/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> postList = postService.findPostByTitle(text);
+        return ResponseEntity.ok().body(postList);
     }
 }
--- projeto-api-Spring-Boot-MongoDB/src/main/java/com/enonnemacher/projetosbmongo/service/PostService.java
+++ projeto-api-Spring-Boot-MongoDB/src/main/java/com/enonnemacher/projetosbmongo/service/PostService.java
@@ -6,6 +6,7 @@ import com.enonnemacher.projetosbmongo.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,5 +18,9 @@ public class PostService {
     public Post findById(String id) {
         Optional<Post> userPost = postRepository.findById(id);
         return userPost.orElseThrow(() -> new NotFoundException("Objeto nao encontrado."));
+    }
+
+    public List<Post> findPostByTitle(String text){
+        return postRepository.findByTitleContainingIgnoreCase(text);
     }
 }
