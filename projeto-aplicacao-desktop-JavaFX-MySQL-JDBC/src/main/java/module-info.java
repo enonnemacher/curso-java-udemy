f5d9a25eb8bdb5c1e2c6dad7c19540de8e46a438
enonnemacher <enonnemacher@gmail.com>
2022-01-12 11:26:05 -0300

    Criado projeto aplicacao-desktop-JavaFX-MySQL-JDBC

---
+++ projeto-aplicacao-desktop-JavaFX-MySQL-JDBC/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.enonnemacher.projetoaplicacaodesktopjavafxmysqljdbc {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.enonnemacher.projetoaplicacaodesktopjavafxmysqljdbc to javafx.fxml;
+    exports com.enonnemacher.projetoaplicacaodesktopjavafxmysqljdbc;
+}
