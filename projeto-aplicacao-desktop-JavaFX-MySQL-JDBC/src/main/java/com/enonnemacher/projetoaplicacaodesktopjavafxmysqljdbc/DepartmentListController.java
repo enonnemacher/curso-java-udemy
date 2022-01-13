2aa8f849342f05f50f72ce61f07b0b2b7cdffa41
enonnemacher <enonnemacher@gmail.com>
2022-01-13 14:21:32 -0300

    Update department

--- projeto-aplicacao-desktop-JavaFX-MySQL-JDBC/src/main/java/com/enonnemacher/projetoaplicacaodesktopjavafxmysqljdbc/DepartmentListController.java
+++ projeto-aplicacao-desktop-JavaFX-MySQL-JDBC/src/main/java/com/enonnemacher/projetoaplicacaodesktopjavafxmysqljdbc/DepartmentListController.java
@@ -5,6 +5,7 @@ import com.enonnemacher.projetoaplicacaodesktopjavafxmysqljdbc.listeners.DataCha
 import com.enonnemacher.projetoaplicacaodesktopjavafxmysqljdbc.service.DepartmentService;
 import com.enonnemacher.projetoaplicacaodesktopjavafxmysqljdbc.util.Alerts;
 import com.enonnemacher.projetoaplicacaodesktopjavafxmysqljdbc.util.Utils;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -12,10 +13,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
 import javafx.stage.Modality;
@@ -38,6 +36,9 @@ public class DepartmentListController implements Initializable, DataChangeListen
 
     @FXML
     private TableColumn<Department, String> tableColumnName;
+
+    @FXML
+    private TableColumn<Department, Department> tableColumnEDIT;
 
     @FXML
     private Button btNew;
@@ -75,6 +76,7 @@ public class DepartmentListController implements Initializable, DataChangeListen
         List<Department> list = departmentService.findAll();
         observableList = FXCollections.observableArrayList(list);
         tableViewDepartment.setItems(observableList);
+        initEditButtons();
     }
 
     private void createDialogForm(Department department, String absoluteName, Stage parentStage) {
@@ -103,5 +105,24 @@ public class DepartmentListController implements Initializable, DataChangeListen
     @Override
     public void onDataChanged() {
         updateTableView();
+    }
+
+    private void initEditButtons() {
+        tableColumnEDIT.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+        tableColumnEDIT.setCellFactory(param -> new TableCell<Department, Department>() {
+            private final Button button = new Button("edit");
+
+            @Override
+            protected void updateItem(Department obj, boolean empty) {
+                super.updateItem(obj, empty);
+                if (obj == null) {
+                    setGraphic(null);
+                    return;
+                }
+                setGraphic(button);
+                button.setOnAction(
+                        event -> createDialogForm(obj, "gui/DepartmentForm.fxml", Utils.currentStage(event)));
+            }
+        });
     }
 }
